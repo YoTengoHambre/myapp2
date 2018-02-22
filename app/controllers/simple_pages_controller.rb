ef4ac24382f6865c1bab01816abc9db0fad78458
YoTengoHambre <michael.flaherty01@gmail.com>
2018-02-21 20:41:49 -0500

    email setup 3

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,16 +8,13 @@ class SimplePagesController < ApplicationController
   def landing_page
   @products = Product.limit(5)
 	end
-
-	def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  ActionMailer::Base.mail(from: @email,
-	      to: 'michael.flaherty01@gmail.com',
-	      subject: "A new contact form message from #{@name}",
-	      body: @message).deliver_now
-	end
+	
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
 end
 
 
