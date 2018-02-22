1825a4d55b9db16c12832f0483844ec6a1bd8ef2
YoTengoHambre <michael.flaherty01@gmail.com>
2018-02-21 20:21:27 -0500

    email setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['app88404212@heroku.com'],
+  password: ENV['zpn0rlmy6369'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
