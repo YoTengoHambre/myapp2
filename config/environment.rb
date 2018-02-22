839a90a0a6223971e4c0cc17b2e1091d9272f090
YoTengoHambre <michael.flaherty01@gmail.com>
2018-02-21 20:28:12 -0500

    email setup 2

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['app88404212@heroku.com'],
-  password: ENV['zpn0rlmy6369'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
