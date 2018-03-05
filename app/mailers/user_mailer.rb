9044659bd1a341c16a8942f0c0efcf06275573f7
YoTengoHambre <michael.flaherty01@gmail.com>
2018-03-04 22:50:24 -0500

    5.11

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,14 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
     @message = message
     @name = name
-      mail(:from => email,
-          :to => 'michael.flaherty01@gmail.com',
-          :subject => "A new contact form message from #{name}")
+    mail(:from => email,
+	 		:to => 'michael.flaherty01@gmail.com',
+	    :subject => "A new contact form message from #{name}")
 	end  
+
+	def welcome(user)
+	  @appname = "Bike Shop"
+	  mail(to: user.email,
+	  	subject: "Welcome to #{@appname}!")
+	end	
 end
