e95da6c7d4a73fecbb095c90b934984e3f314b7f
YoTengoHambre <michael.flaherty01@gmail.com>
2018-03-07 22:17:48 -0500

    6.6

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
 	post 'simple_pages/thank_you'
 
+	post 'payments/create'
+
 	#root 'simple_pages#index'
 	resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
