9ef83ff5f3e8cb04870d31a8ca32bc4c5f03d718
YoTengoHambre <michael.flaherty01@gmail.com>
2018-03-15 16:01:13 -0400

    deploy to heroku to test jquery

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
 	post 'payments/create'
 
+	mount ActionCable.server => '/cable'
 	#root 'simple_pages#index'
 	resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
