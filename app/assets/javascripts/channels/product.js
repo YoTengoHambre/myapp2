9099c0d8d77733e10d2c0a5f655f93798992307e
YoTengoHambre <michael.flaherty01@gmail.com>
2018-03-23 18:13:58 -0400

    14.6 post review text through ActionCable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,13 +7,13 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     
   },
 
-  received: function(data) {
-    $(".alert.alert-info").show();
-    $(".new-product-review").prepend(data.comment);
-    $("#average-rating").attr('data-score', data.average_rating);
-    refreshRating();
-    
-  },
+received: function(data) {
+  // Called when there's incoming data on the websocket for this channel
+  $(".alert.alert-info").show();
+  $('.product-reviews').prepend(data.comment);
+  $("#average-rating").attr('data-score', data.average_rating);
+  refreshRating();
+},
   listen_to_comments: function(){
     return this.perform('listen',{
       product_id: $("[data-product-id]").data("product-id")
