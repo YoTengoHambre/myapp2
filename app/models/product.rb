39a0a5af8c3e5bc744d2d6ce2f9ed407668718d9
YoTengoHambre <michael.flaherty01@gmail.com>
2018-03-24 01:45:30 -0400

    5.2 revision revision

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,9 @@ class Product < ApplicationRecord
 	
 	def self.search(search_term)
 		if Rails.env != "production"
-			Product.where("name || colour || description LIKE ?", "%#{search_term}%")
+			Product.where("name || color || description LIKE ?", "%#{search_term}%")
 		else
-			Product.where("name || colour || description iLIKE ?", "%#{search_term}%")
+			Product.where("name || color || description iLIKE ?", "%#{search_term}%")
 		end
 	end
 	
